485b42e5fda359fff6741593e505edf9c4181334
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-02-04 20:40:51 -0600

    make links active

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,7 @@
 const Login = () => {
   return (
     <>
-      <div class='jumbotron bg-primary py-5 d-flex justify-content-center display-1 text-light fw-bold shadow'>
+      <div className='jumbotron bg-primary py-5 d-flex justify-content-center display-1 text-light fw-bold shadow'>
         Login
       </div>
       <p>login page</p>
